847da5082746fc3f4a31c182ba3b7830d017330e
PietroRobbiano <100785610+PietroRobbiano@users.noreply.github.com>
2022-05-08 20:25:53 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,9 @@
-
+int numeromaggiore( int array[], int dimensione ){
+    int b=0;
+    for ( int i=0 ; i<dimensione ; i++){
+        if ( b <= array[i]) {
+            b=array[i];
+        }
+    }
+    return b;
+}
